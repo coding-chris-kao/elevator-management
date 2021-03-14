b9748e2e74d179537203b7b100d04dc9c4dc6bad
Chris Kao <chris.kao@cadmen.com>
2021-03-15 01:10:37 +0800

    Finish Simulator class

--- models/Simulator.ts
+++ models/Simulator.ts
@@ -1,14 +1,18 @@
 import { Building } from './Building'
+import { EfficentControlStrategy } from './ControlStrategy'
 import { Elevator } from './Elevator'
 import { Floor } from './Floor'
 import { Person } from './Person'
 
 export class Simulator {
+  public simulationPeriod = 1000
+  public totlePeople = 40
   public floorCount: number = 10
-  public elevatorCount: number = 2
+  public elevatorCount: number = 1
   public elevatorLimit: number = 5
   public elevatorTravelTime: number = 1000
   public elevatorExchangeTime: number = 1000
+  public callElevatorDelay: number = 1000
 
   public start() {
     // Create building
@@ -33,24 +37,53 @@ export class Simulator {
       building.registerElevator(elevator)
     }
 
-    // Create person
-    this.createPerson(1, building)
+    // Create person and run the simulation
+    let personId = 1
+    const startTime = Date.now()
+    const simulationTimer = setInterval(() => {
+      const person = this.createPerson(personId, building)
+      person.callElevator(this.callElevatorDelay)
+      personId++
+      if (personId > this.totlePeople) {
+        clearInterval(simulationTimer)
+        let elapsedTime = Date.now() - startTime
+        console.info(`模擬結束，花費時間： ${elapsedTime} millisecond`)
+      }
+    }, this.simulationPeriod)
+
+    // Test
+    // this.testCreatePerson(building)
 
     // The Building start monitoring
-    building.startMonitor()
+    building.startMonitor(new EfficentControlStrategy())
+  }
+
+  public testCreatePerson(building: Building) {
+    let from, to
+
+    from = 0
+    to = 9
+    const person1 = new Person(1, building.floors[from], building.floors[to])
+    person1.callElevator(this.callElevatorDelay)
+
+    from = 1
+    to = 3
+    const person2 = new Person(2, building.floors[from], building.floors[to])
+    person2.callElevator(this.callElevatorDelay)
+
+    from = 2
+    to = 8
+    const person3 = new Person(3, building.floors[from], building.floors[to])
+    person3.callElevator(this.callElevatorDelay)
   }
 
   public createPerson(id: number, building: Building): Person {
-    // let from = Math.floor(Math.random() * this.floorCount)
-    // let to
-    // do {
-    //   to = Math.floor(Math.random() * this.floorCount)
-    // } while (to == from)
-    let from = 5
-    let to = 3
-    const person = new Person(1, building.floors[from], building.floors[to])
-    person.callElevator()
-
+    let from = Math.floor(Math.random() * this.floorCount)
+    let to
+    do {
+      to = Math.floor(Math.random() * this.floorCount)
+    } while (to == from)
+    const person = new Person(id, building.floors[from], building.floors[to])
     return person
   }
 }
